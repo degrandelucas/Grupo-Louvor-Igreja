0563e6a12c39ccb2a94b171bd1a890697b056381
degrandelucas <lucasdegrande15@gmail.com>
2025-04-01 16:49:20 -0300

    criando um arquivo javascript para renderizacao dos louvores por container dentro do html, cada array const com objetos contendo titulo, linkLetra e linkVideo

---
+++ script-louvores.js
@@ -0,0 +1,255 @@
+const louvoresEnsaiados = [
+    {
+        titulo: 'Pode Morar Aqui - Theo Rubia',
+        linkLetra: 'https://www.letras.mus.br/theo-rubia/pode-morar-aqui/',
+        linkVideo: 'https://www.youtube.com/watch?v=n0fDvJAyrQ8'
+    },
+    {
+        titulo: 'Oh, Quão Lindo Esse Nome É - Ana Nóbrega',
+        linkLetra: 'https://www.letras.mus.br/ana-nobrega/oh-quao-lindo-esse-nome-e-what-a-beautiful-name/',
+        linkVideo: 'https://www.youtube.com/watch?v=8XUqcxgH8r4'
+    },
+    {
+        titulo: 'Derrama o Teu Poder - Bruna Olly',
+        linkLetra: 'https://www.letras.mus.br/bruna-olly/derrama-o-teu-poder-part-julliany-souza-e-leo-brandao/',
+        linkVideo: 'https://www.youtube.com/watch?v=VL0cNb_psdw'
+    },
+    {
+        titulo: 'Yahweh Se Manifestará - Marcos Freire',
+        linkLetra: 'https://www.letras.mus.br/marcos-freire/yahweh-se-manifestara-part-julliany-souza-e-leo-brandao/',
+        linkVideo: 'https://www.youtube.com/watch?v=EVot5doOp9Q'
+    },
+    {
+        titulo: 'Bondade de Deus - Isaías Saad',
+        linkLetra: 'https://www.letras.mus.br/isaias-saad/bondade-de-deus/',
+        linkVideo: 'https://www.youtube.com/watch?v=mZ9yZYo9Mmk'
+    },
+    {
+        titulo: 'Caminho No Deserto - Soraya Moraes',
+        linkLetra: 'https://www.letras.mus.br/soraya-moraes/caminho-no-deserto-way-maker/',
+        linkVideo: 'https://www.youtube.com/watch?v=AAvjEp7u9tA&t=97s'
+    },
+    {
+        titulo: 'A Boa Parte - Nívea Soares',
+        linkLetra: 'https://www.letras.mus.br/florianopolis-house-of-prayer/a-boa-parte/',
+        linkVideo: 'https://www.youtube.com/watch?v=dlGOiuxSzVw'
+    },
+    {
+        titulo: 'Acende Outra Vez - Jefferson & Suellen',
+        linkLetra: 'https://www.letras.mus.br/jefferson-e-suellen/acenda-outra-vez/',
+        linkVideo: 'https://www.youtube.com/watch?v=86cC5jqPPBI'
+    },
+    {
+        titulo: 'Cordeiro e Leão - Jefferson & Suellen',
+        linkLetra: 'https://www.letras.mus.br/jefferson-e-suellen/cordeiro-e-leao/',
+        linkVideo: 'https://www.youtube.com/watch?v=SKS7zkEEqrM'
+    },
+    {
+        titulo: 'Deus Dos Deuses - Lauriete',
+        linkLetra: 'https://www.letras.mus.br/lauriete/173210/',
+        linkVideo: 'https://www.youtube.com/watch?v=XCir1hpYhn8'
+    },
+    {
+        titulo: 'Sonda-me, Usa-me - Aline Barros',
+        linkLetra: 'https://www.letras.mus.br/aline-barros/1131959/',
+        linkVideo: 'https://www.youtube.com/watch?v=1McZiORjlzg'
+    },
+    {
+        titulo: 'João 20 / Pra Sempre / Porquê Ele Vive - Josué Freitas',
+        linkLetra: 'https://www.letras.mus.br/josue-freitas/joao-20-pra-sempre/',
+        linkVideo: 'https://www.youtube.com/watch?v=KSXBGjb6TyM'
+    },
+    {
+        titulo: 'Ruja o Leão / Que Se Abram Os Céus - Isaías Saad',
+        linkLetra: 'https://www.letras.mus.br/isaias-saad/ruja-o-leao-que-se-abram-os-ceus-part-nivea-soares/',
+        linkVideo: 'https://www.youtube.com/watch?v=gTRFVMkMajw'
+    },
+    {
+        titulo: 'Enche-me - Isaías Saad',
+        linkLetra: 'https://www.letras.mus.br/isaias-saad/enche-me-part-gabriela-rocha/',
+        linkVideo: 'https://www.youtube.com/watch?v=ozBlvwVizXI'
+    },
+    {
+        titulo: 'Oceanos - Ana Nóbrega',
+        linkLetra: 'https://www.letras.mus.br/ana-nobrega/oceanos-oceans/',
+        linkVideo: 'https://www.youtube.com/watch?v=rNA8QlQI_dI'
+    },
+    {
+        titulo: 'Santo Pra Sempre - Ana Nóbrega',
+        linkLetra: 'https://www.letras.mus.br/ana-nobrega/santo-pra-sempre/',
+        linkVideo: 'https://www.youtube.com/watch?v=-841Zit4t30'
+    },
+    {
+        titulo: 'Liberta-me de Mim - Luma Elpidio',
+        linkLetra: 'https://www.letras.mus.br/luma-elpidio/liberta-me-de-mim/',
+        linkVideo: 'https://www.youtube.com/watch?v=Ot8KIh6-uEU'
+    }
+];
+
+const louvoresProximoEnsaio = [
+    {
+        titulo: 'Quem É Esse? - Julliany Souza',
+        linkLetra: 'https://www.letras.mus.br/julliany-souza/quem-e-esse/',
+        linkVideo: 'https://www.youtube.com/watch?v=0ZF5em0MTwY'
+    },
+    {
+        titulo: 'Único - Fernandinho',
+        linkLetra: 'https://www.letras.mus.br/fernandinho/unico/',
+        linkVideo: 'https://www.youtube.com/watch?v=xFN1kAW4xqk'
+    },
+    {
+        titulo: 'Rendido Estou - Isaías Saad',
+        linkLetra: 'https://www.letras.mus.br/isaias-saad/rendido-estou-part-debora-buzas/',
+        linkVideo: 'https://www.youtube.com/watch?v=EhRoefQgV_U'
+    },
+    {
+        titulo: 'Te Louvarei / Aclame Ao Senhor - Guilherme Baptista',
+        linkLetra: 'https://www.letras.mus.br/guilherme-baptista/te-louvarei-aclame-ao-senhor-medley/',
+        linkVideo: 'https://www.youtube.com/watch?v=doA85EYrfPc'
+    },
+    {
+        titulo: 'Cadeias Quebrar - Sarah Oliveira',
+        linkLetra: 'https://www.letras.mus.br/sarah-oliveira/cadeias-quebrar/',
+        linkVideo: 'https://www.youtube.com/watch?v=OfWszjc6LC8'
+    },
+    {
+        titulo: 'Escape / Nunca Pare de Lutar - Jadson Moreno',
+        linkLetra: 'https://www.letras.mus.br/jadson-moreno/escape-nunca-pare-de-lutar/',
+        linkVideo: 'https://www.youtube.com/watch?v=kAwznSYA_oE'
+    },
+    {
+        titulo: 'Águas Purificadoras / Oceanos - Isaías Saad',
+        linkLetra: 'https://www.letras.mus.br/isaias-saad/aguas-purificadoras-oceanos/',
+        linkVideo: 'https://www.youtube.com/watch?v=XUDifaNsjtA'
+    }
+];
+
+const louvoresFuturo = [
+    {
+        titulo: 'Meu Respirar / Meu Prazer - Gabriela Rocha',
+        linkLetra: 'https://www.letras.mus.br/gabriela-rocha/meu-prazer-meu-respirar/',
+        linkVideo: 'https://www.youtube.com/watch?v=hrqfviD97r0'
+    },
+    {
+        titulo: 'Essência da Adoração - David Quinlan',
+        linkLetra: 'https://www.letras.mus.br/david-quinlan/essencia-da-adoracao-part-lu-alone/',
+        linkVideo: 'https://www.youtube.com/watch?v=lxo6Fgm_n38'
+    },
+    {
+        titulo: 'Ao Único - Aline Barros',
+        linkLetra: 'https://www.letras.mus.br/aline-barros/178166/',
+        linkVideo: 'https://www.youtube.com/watch?v=60CuZyzGf5U'
+    },
+    {
+        titulo: 'Deus Está Aqui / O Espírito de Deus / Manancial - Gabriela Rocha',
+        linkLetra: 'https://www.letras.mus.br/gabriela-rocha/deus-esta-aqui/',
+        linkVideo: 'https://www.youtube.com/watch?v=2Xs4Qq_wr3k'
+    },
+    {
+        titulo: 'Atos 2 / Ele É Exaltado - Gabriel Guedes',
+        linkLetra: 'https://www.letras.mus.br/gabriel-guedes/atos-2-ele-e-exaltado-medley/',
+        linkVideo: 'https://www.youtube.com/watch?v=Stq7QCZT0rY&t=1s'
+    },
+    {
+        titulo: 'Eu Vou Construir - Juliano Son',
+        linkLetra: 'https://www.letras.mus.br/juliano-son/eu-vou-construir/',
+        linkVideo: 'https://www.youtube.com/watch?v=1htnqzaKLP8'
+    },
+    {
+        titulo: 'Vitorioso És - Gabriel Guedes',
+        linkLetra: 'https://www.letras.mus.br/gabriel-guedes/vitorioso-es/',
+        linkVideo: 'https://www.youtube.com/watch?v=k7tGP-vidwc'
+    },
+    {
+        titulo: 'Nada Mais - Gabriel Guedes',
+        linkLetra: 'https://www.letras.mus.br/gabriel-guedes/nada-mais/',
+        linkVideo: 'https://www.youtube.com/watch?v=OUyLQ4LwEBE'
+    },
+    {
+        titulo: 'Poderoso Deus / O Que Tua Glória Fez Comigo / Preciso de Ti - Gabriela Rocha',
+        linkLetra: 'https://www.letras.mus.br/gabriela-rocha/poderoso-deus-o-que-tua-gloria-fez-comigo-preciso-de-ti-medley/',
+        linkVideo: 'https://www.youtube.com/watch?v=_L6a96-XOyI'
+    },
+    {
+        titulo: 'Poderosamente Vivo - Sandra Pires',
+        linkLetra: 'https://www.letras.mus.br/sandra-pires/poderosamente-vivo/',
+        linkVideo: 'https://www.youtube.com/watch?v=rhNLlFmRHK4'
+    },
+    {
+        titulo: 'Ele Vem - Gabriel Guedes',
+        linkLetra: 'https://www.letras.mus.br/gabriel-guedes/ele-vem/',
+        linkVideo: 'https://www.youtube.com/watch?v=YudXB8MZRTs'
+    },
+    {
+        titulo: 'Clamo Jesus - Paulo César Baruk',
+        linkLetra: 'https://www.letras.mus.br/paulo-cesar-baruk/clamo-jesus/',
+        linkVideo: 'https://www.youtube.com/watch?v=ye9caqETC0A'
+    },
+    {
+        titulo: 'Digno de Tudo - Sarah Oliveira',
+        linkLetra: 'https://www.letras.mus.br/sarah-oliveira/digno-de-tudo/',
+        linkVideo: 'https://www.youtube.com/watch?v=OyDKAhdAP9U'
+    }
+];
+
+function renderizarLouvores(louvores, idUlLista) {
+    const lista = document.getElementById(idUlLista);
+    lista.innerHTML = ''; // Limpa a lista antes de adicionar novos itens abaixo
+
+    louvores.forEach(louvor => {
+        const li = document.createElement('li');
+        li.className = 'collection-item';
+
+        const div = document.createElement('div');
+
+        const titulo = document.createTextNode(louvor.titulo);
+        div.appendChild(titulo);
+
+        const linkLetra = document.createElement('a');
+        linkLetra.href = louvor.linkLetra;
+        linkLetra.target = '_blank';
+        linkLetra.className = 'secondary-content first-icon';
+
+        const iconeLetra = document.createElement('i');
+        iconeLetra.className = 'material-icons';
+        iconeLetra.textContent = 'text_fields';
+        linkLetra.appendChild(iconeLetra);
+
+        const linkVideo = document.createElement('a');
+        linkVideo.href = louvor.linkVideo;
+        linkVideo.target = '_blank';
+        linkVideo.className = 'secondary-content second-icon red-text';
+
+        const iconeVideo = document.createElement('i');
+        iconeVideo.className = 'material-icons';
+        iconeVideo.textContent = 'ondemand_video';
+        linkVideo.appendChild(iconeVideo);
+
+        div.appendChild(linkLetra);
+        div.appendChild(linkVideo);
+        li.appendChild(div);
+        lista.appendChild(li); // Adiciona o item à lista
+    });
+}
+
+function ordenarLouvores(louvores) {
+    return louvores.sort((a, b) => {
+        const tituloA = a.titulo.toUpperCase(); // Ignora maiúsculas e minúsculas
+        const tituloB = b.titulo.toUpperCase(); // Ignora maiúsculas e minúsculas
+        if (tituloA < tituloB) {
+            return -1;
+        }
+        if (tituloA > tituloB) {
+            return 1;
+        }
+        return 0; // Os títulos são iguais
+    });
+}
+
+ordenarLouvores(louvoresEnsaiados);
+ordenarLouvores(louvoresProximoEnsaio);
+ordenarLouvores(louvoresFuturo);
+
+renderizarLouvores(louvoresEnsaiados, 'lista-louvores-ensaidos');
+renderizarLouvores(louvoresProximoEnsaio, 'lista-proximo-ensaio');
+renderizarLouvores(louvoresFuturo, 'lista-louvores-futuro');
