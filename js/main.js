7f0997da11540d443d716027c70bad146321a38d
degrandelucas <lucasdegrande15@gmail.com>
2025-04-03 21:21:16 -0300

    criando um funcao generica em utilidades.js para atualizar o ano automatica do copyright conforme ano atual da classe Date, importando essa funcao em main.js e usando o id dentro do span no index.html para representar a resposta do ano

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 import {louvoresEnsaiados, louvoresProximoEnsaio, louvoresFuturo} from './script-louvores.js';
 import {ordenarLouvores} from './ordenacao.js';
 import {renderizarLouvores} from './renderizacao.js';
+import {atualizarAnoCopyright} from './utilidades.js';
 
 ordenarLouvores(louvoresEnsaiados);
 ordenarLouvores(louvoresProximoEnsaio);
@@ -11,6 +12,8 @@ ordenarLouvores(louvoresFuturo);
 renderizarLouvores(louvoresEnsaiados, 'lista-louvores-ensaidos');
 renderizarLouvores(louvoresProximoEnsaio, 'lista-proximo-ensaio');
 renderizarLouvores(louvoresFuturo, 'lista-louvores-futuro');
+
+atualizarAnoCopyright('ano-copyright');
 
 // script-materialize.js para o framwork Materialize CSS, apos carregamento completo do DOM
 import './script-materialize.js';
--- js/utilidades.js
+++ js/utilidades.js
@@ -3,3 +3,9 @@
 export function removerAcentos(texto) {
     return texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
+
+export function atualizarAnoCopyright(idAnoCopyright) {
+    const anoAtual = new Date().getFullYear();
+    const anoCopyright = document.getElementById(idAnoCopyright);
+    anoCopyright.textContent = anoAtual;
+}
